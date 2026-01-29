fdad6bcc3b0eabb013427202748f8d0fab2fc513
John Mark <johnmarkroco05@gmail.com>
2026-01-29 23:46:21 +0800

    Update djangoblog_venv_wsgi.py

--- djangoblog_venv_wsgi.py
+++ djangoblog_venv_wsgi.py
@@ -1,11 +1,15 @@
 import os
 import sys
-
-path = 'djangoblog/'
-if path not in sys.path
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTING_MODULE'] = 'djangoblog.settings'
-
+#
+## assuming your django settings file is at '/home/pollyolly/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/pollyolly/mysite/manage.py'
+path = '/home/pollyolly/djangoblog'
+if path not in sys.path:
+    sys.path.insert(0, path)
+    # sys.path.append(path)
+#
+os.environ['DJANGO_SETTINGS_MODULE'] = 'djangoblog.settings'
+#
+## then:
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
